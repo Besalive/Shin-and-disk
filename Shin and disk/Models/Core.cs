dabb10ebaa847ba3724a0165f855e9f3f4fa0a92
user1 <user1@mail.ru>
2022-11-28 15:51:32 +0500

    Добавьте файлы проекта.

---
+++ Shin and disk/Models/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shin_and_disk.Models
+{
+    class Core
+    {
+        TiresCompanyEntities context = new TiresCompanyEntities();
+    }
+}
